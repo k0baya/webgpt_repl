afc3ab480e8a54e5ae6dd5c70273869150f49d1c
k0baya <132335755+k0baya@users.noreply.github.com>
2023-06-12 12:02:58 +0800

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,1 @@
+git clone https://github.com/Chanzhaoyu/chatgpt-web web && cd web && npm install pnpm -g && pnpm bootstrap && cd service && pnpm install && cd /home/runner/${REPL_SLUG} && chmod +x web.sh
