fdceaf48e4ba3249eb4146f699008a9b37b0a597
k0baya <132335755+k0baya@users.noreply.github.com>
2023-06-12 11:42:53 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+const { spawn } = require('child_process');
+
+const script = spawn('sh', ['web.sh']);
+
+script.stdout.on('data', (data) => {
+  console.log(` ${data}`);
+});
+
+script.stderr.on('data', (data) => {
+  console.error(` ${data}`);
+});
+
+script.on('close', (code) => {
+  console.log(`Shell Process exited with code ${code}`);
+});
