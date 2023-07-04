230d2b98600ef866471b1f44b4de3aae9205d06d
k0baya <132335755+k0baya@users.noreply.github.com>
2023-07-04 19:19:25 +0800

    Update install.sh

--- pandora/install.sh
+++ pandora/install.sh
@@ -1,6 +1,6 @@
 echo "import os" > main.py
 echo "status = os.system('sh start.sh')" >> main.py
-cat << EOF > start_example.sh
+cat << EOF > start.sh
 TOKEN=\${TOKEN:-'pk-this-is-a-real-free-pool-token-for-everyone'}
 echo "\${TOKEN}" > tokens.txt
 pip install pandora-chatgpt pandora-chatgpt[api] pandora-chatgpt[cloud] --upgrade
